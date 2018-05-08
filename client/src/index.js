fbd0074a4167b05437f165da223d83ec73030d3a
zssiddique <zssiddique@gmail.com>
2018-05-08 19:37:44 -0400

    made the routing work with inventory

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
-import registerServiceWorker from './registerServiceWorker';
-import fontawesome from "@fortawesome/fontawesome";
-import freeSolidIcons from "@fortawesome/fontawesome-free-solid";
 
-fontawesome.library.add(freeSolidIcons);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
