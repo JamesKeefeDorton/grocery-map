2a80147ef6471037747ce8e10e289554ae24718d
JamesKeefeDorton <jameskeefedorton@gmail.com>
2018-05-09 19:49:27 -0400

    compatibility changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ListItems from "./pages/ListItems";
 import Nav from "./components/Nav";
-import Register from "./components/Register";
-import Login from "./components/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => (
   <Router>
