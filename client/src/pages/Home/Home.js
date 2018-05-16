1ac8eb2cc91489ee0275578b52ee6d4df99e8bd8
zssiddique <zssiddique@gmail.com>
2018-05-16 12:42:57 -0400

    added

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+	componentDidMount() {
+		console.log("ended here from map list ", this.props.location.state.detail);
+	}
 	render() {
 		return (
 			<h3>Home</h3>
